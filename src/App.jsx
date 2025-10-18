2b67b5dac22b79001ef3a8b99850ab064fc67e6c
Shubhanshu <shbhansh66@gmail.com>
2025-10-18 23:36:48 +0530

    use deltaY in scroling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
 const App = () => {
+
+  function scrolling(elem){
+    if(elem>0){
+      console.log("up_scroling...")
+    }
+    else{
+      console.log("down-scroling...")
+    }
+  }
+
+
   return (
     <div className=''>
 
@@ -10,6 +21,16 @@ const App = () => {
     <button onMouseLeave={function(elem){
      console.log('hellow')
     }} className='p-4 border-2 m-10 bg-gray-600 rounded-4xl shadow-lg'>Change me</button>
+
+
+
+    <div onWheel={function(elem){
+       scrolling(elem.deltaY);
+    }}>
+      <div className="h-[100vh] w-full bg-green-500"></div>
+      <div className="h-[100vh] w-full bg-gray-700"></div>
+      <div className="h-[100vh] w-full bg-amber-200"></div>
+    </div>
     </div>
   )
 }
